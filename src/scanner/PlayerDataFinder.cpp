e198fb2551e607b04b1d56c2bae8b231a546f120
RiiK26 <syakirr266@gmail.com>
2026-09-15 03:52:44 +0700

    fix: increase upper bound of player data value range to 260 for additional runes updates

--- src/scanner/PlayerDataFinder.cpp
+++ src/scanner/PlayerDataFinder.cpp
@@ -174,7 +174,7 @@ std::optional<PlayerDataResult> PlayerDataFinder::Find()
 
                 for (size_t i = Il2CppDictOffsets::Count; reg.RegionSize >= 0x30 && i < reg.RegionSize - 0x30; i += 4) {
                   int32_t val = *reinterpret_cast<int32_t*>(&buffer[i]);
-                  if (val >= 200 && val <= 250) {
+                  if (val >= 200 && val <= 260) {
                     size_t dictOffset = i - Il2CppDictOffsets::Count;
 
                     int32_t freeCount = *reinterpret_cast<int32_t*>(&buffer[dictOffset + Il2CppDictOffsets::FreeCount]);
